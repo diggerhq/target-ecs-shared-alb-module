1823fff0afdd3c3e95b360cc78f181d7f49a2cb2
alexey <alexey@digger.dev>
2023-07-10 15:58:13 +0100

    add shared cluster

--- cluster.template.tf
+++ cluster.template.tf
@@ -1,6 +1,6 @@
 {% if shared_ecs_cluster is defined and shared_ecs_cluster %}
   data "aws_ecs_cluster" "ecs_cluster" {
-    cluster_name = "{{shared_ecs_cluster}}"
+    cluster_name = "{{shared_ecs_cluster_name}}"
   }
 {% else %}
 resource "aws_ecs_cluster" "ecs_cluster" {
